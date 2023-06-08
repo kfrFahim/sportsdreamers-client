a1da216faa47361d61dee0efda9a61a01f2a9677
kfrFahim <kfrfahim3@gmail.com>
2023-06-08 20:00:48 +0600

    private route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate, useLocation } from 'react-router'
+import Loader from '../components/Shared/Loader'
+import { useContext } from 'react'
+import { AuthContext } from '../providers/AuthProvider'
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext)
+  const location = useLocation()
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (user) {
+    return children
+  }
+  return <Navigate to='/login' state={{ from: location }} replace></Navigate>
+}
+
+export default PrivateRoute
