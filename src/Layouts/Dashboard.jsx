0a447fab0669b08fc37cc66b89693c6fd0afeb97
kfrFahim <kfrfahim3@gmail.com>
2023-06-09 00:28:04 +0600

    my carts

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Bounce } from "react-awesome-reveal";
+import { FaShoppingCart,FaWallet,FaShoppingBasket,FaHome,FaBars } from 'react-icons/fa';
 
 const Dashboard = () => {
      return (
@@ -14,10 +16,17 @@ const Dashboard = () => {
   </div> 
   <div className="drawer-side">
     <label htmlFor="my-drawer-2" className="drawer-overlay"></label> 
-    <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+    <ul className="menu p-4 w-80 h-full bg-gray-50 text-black">
       {/* Sidebar content here */}
-      <li><Link to="">My Selected Classes</Link></li>
-      <li><a>Sidebar Item 2</a></li>
+      <li><NavLink to="/dashboard/myclasses">  <Bounce> <FaShoppingCart></FaShoppingCart> </Bounce> My Selected Classes</NavLink></li>
+      <li><NavLink to="/dashboard/myenrollclasses">  <Bounce> <FaShoppingBasket></FaShoppingBasket> </Bounce> My Enrolled Classes</NavLink></li>
+      <li><NavLink to="/dashboard/paymenthistory">  <Bounce> <FaWallet></FaWallet> </Bounce>Payment History </NavLink></li>
+      <div className="divider"></div>
+
+      <li><NavLink to="/">  <Bounce> <FaHome></FaHome></Bounce> Home </NavLink></li>
+      <li><NavLink to="/classes">  <Bounce> <FaBars></FaBars></Bounce> All Classes </NavLink></li>
+      <li><NavLink to="/">  <Bounce> <FaHome></FaHome></Bounce> Home </NavLink></li>
+      <li><NavLink to="/">  <Bounce> <FaHome></FaHome></Bounce> Home </NavLink></li>
     </ul>
   
   </div>
--- src/pages/Dashboard/MyClasses/MyClasses.jsx
+++ src/pages/Dashboard/MyClasses/MyClasses.jsx
@@ -1,10 +1,74 @@
 import React from 'react';
+import useCart from '../../../hooks/useCart';
+import { Helmet } from 'react-helmet';
+import { FaTrashAlt } from "react-icons/fa";
+import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
 
 const MyClasses = () => {
+     const [cart] = useCart();
+
+     const total = cart.reduce((sum, item) => item.price + sum, 0);
      return (
-          <div>
-               <h2>MY Classes</h2>
+          <div className="w-full p-4">
+          <Helmet>
+            <title>SportsDremars | My Cart </title>
+          </Helmet>
+          <div className="flex justify-between uppercase font-semibold">
+            <h2>My cart {cart.length}</h2>
+            <h2>Total Price : ${total}</h2>
+           <Link to="/dashboard/payment"> <button className="btn btn-warning btn-sm">PAY</button></Link>
           </div>
+    
+          {/* Table */}
+    
+          <div className="overflow-x-auto w-full">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>
+            <th> # </th>
+            </th>
+            <th>Item Image</th>
+            <th>Item Name</th>
+            <th>Price</th>
+            <th>Delete</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            cart.map((items , index) => <tr>
+              <th> {index + 1} </th>
+              <td>
+              <div className="flex items-center space-x-3">
+                <div className="avatar">
+                  <div className="mask mask-squircle w-12 h-12">
+                    <img src={items.image} />
+                  </div>
+                </div>
+                </div>
+              </td>
+              <td>
+               {items.name}
+                {/* <br/>
+                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span> */}
+              </td>
+              <td>$ {items.price}</td>
+
+              <th>
+                <button onClick={() => handleDelete(items)} className="btn btn-ghost bg-red-500 text-white"><FaTrashAlt></FaTrashAlt></button>
+              </th>
+            </tr> )
+          }
+    
+        </tbody>
+        
+      </table>
+    </div>
+    
+        </div>
      );
 };
 
